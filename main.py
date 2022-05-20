630e5c142007da103b57fa234620598530b6fb3e
Zvika Haramaty <haramaty.zvika@gmail.com>
2022-05-20 16:59:03 +0300

    pressure; cursor

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ window = pyglet.window.Window(1024, 768, caption="PenBoard", resizable=True)
 batch = pyglet.graphics.Batch()
 pyglet.gl.glClearColor(0.9, 0.9, 0.9, 1.0)
 
+cursor = window.get_system_mouse_cursor(window.CURSOR_CROSSHAIR)
+window.set_mouse_cursor(cursor)
+
+
 tablets = pyglet.input.get_tablets()
 canvases = []
 
@@ -36,7 +40,7 @@ else:
 def on_motion(cursor, x, y, pressure, tilt_x, tilt_y):
     if pressure:
         print('%s: on_motion(%r, %r, %r, %r, %r, %r)' % (name, cursor, x, y, pressure, tilt_x, tilt_y))
-        point = pyglet.shapes.Circle(x=x, y=y, radius=6, color=(0, 0, 0), batch=batch)
+        point = pyglet.shapes.Circle(x=x, y=y, radius=6 * (pressure / 4 + 0.5), color=(0, 0, 0), batch=batch)
         points.append(point)
 
 
