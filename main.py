a922fed0474e27402b7dd8aa1dfff2a84d93d4aa
Zvika Haramaty <haramaty.zvika@gmail.com>
2022-05-23 00:06:47 +0300

    color chooser

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # TODO: eraser O(1)
-# TODO: colors
 # TODO: scrolling/pages
 # TODO: resizing
 
@@ -7,7 +6,8 @@
 # TODO: save
 # TODO: create PDF
 
-# TODO: mouse - color chooser
+# TODO: better colors to choose from
+# TODO: keyboard shortcuts (explain how to choose color)
 # TODO: change brush size
 # TODO: readme
 
@@ -25,6 +25,8 @@ board = Board(window)
 canvas = tablet.open_tablet(window)
 pen = {}
 
+change_mouse_cursor('default', window, board)
+
 if canvas is not None:
     @canvas.event
     def on_motion(cursor, x, y, pressure, tilt_x, tilt_y, buttons):
@@ -38,9 +40,9 @@ if canvas is not None:
         }
         if pressure == 0:
             if cursor.name == "Eraser":
-                change_mouse_cursor(window.CURSOR_NO, window, 8)
+                change_mouse_cursor('eraser', window, board, width=8)
             else:
-                change_mouse_cursor(window.CURSOR_DEFAULT, window)
+                change_mouse_cursor('default', window, board)
 
 
 @window.event
@@ -71,34 +73,34 @@ def combined_buttons(button):
 def on_mouse_press(x, y, button, modifiers):
     if combined_buttons(button) == 'click':
         if board.color_chooser is not None:
-            board.color_chooser.handle_click(x ,y)
+            board.color_chooser.handle_click(x, y)
         else:
-            change_mouse_cursor(window.CURSOR_CROSSHAIR, window)
+            change_mouse_cursor('pen', window, board)
             operations.paint(board, pen.get('pressure', 0.4), x, y, (x, y))
             on_mouse_drag.prev_point = (x, y)
     elif combined_buttons(button) == 'erase':
         width = operations.erase(board, pen.get('pressure', 0.5), x, y)
-        change_mouse_cursor(window.CURSOR_NO, window, width)
+        change_mouse_cursor('eraser', window, board, width=width)
     elif combined_buttons(button) == 'select':
         ColorChooser(board, x, y)
 
 
 @window.event
 def on_mouse_release(x, y, button, modifiers):
-    change_mouse_cursor(window.CURSOR_DEFAULT, window)
+    change_mouse_cursor('default', window, board)
     on_mouse_drag.prev_point = None
 
 
 @window.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
     if combined_buttons(buttons) == 'click':
-        change_mouse_cursor(window.CURSOR_CROSSHAIR, window)
+        change_mouse_cursor('pen', window, board)
         if on_mouse_drag.prev_point is not None:
             operations.paint(board, pen.get('pressure', 0.6), x, y, on_mouse_drag.prev_point)
         on_mouse_drag.prev_point = (x, y)
     elif combined_buttons(buttons) == 'erase':
         width = operations.erase(board, pen.get('pressure', 0.5), x, y)
-        change_mouse_cursor(window.CURSOR_NO, window, width)
+        change_mouse_cursor('eraser', window, board, width=width)
 
 
 on_mouse_drag.prev_point = None
--- mouse_cursor.py
+++ mouse_cursor.py
@@ -11,13 +11,26 @@ class EraserMouseCursor(pyglet.window.MouseCursor):
         circle.opacity = 128
         batch.draw()
 
+class ColoredMouseCursor(pyglet.window.MouseCursor):
+    def __init__(self, color):
+        self.color = color
+        self.size = 40
+        self.width = 3
 
-def change_mouse_cursor(type, window, width=None):
-    # if type in [window.CURSOR_CROSSHAIR,]:
-    #     image = pyglet.image.load('pencil.png')
-    #     cursor = pyglet.window.ImageMouseCursor(image, 16, 8)
-    if type == window.CURSOR_NO:
+    def draw(self, x, y):
+        batch = pyglet.graphics.Batch()
+        self.line1 = pyglet.shapes.Line(x-self.size/2, y, x+self.size/2, y, width=self.width, color=self.color, batch=batch)
+        self.line2 = pyglet.shapes.Line(x, y-self.size/2, x, y+self.size/2,  width=self.width, color=self.color, batch=batch)
+        batch.draw()
+
+
+def change_mouse_cursor(type, window, board, width=None):
+    if type == 'eraser':
         cursor = EraserMouseCursor(width)
+    elif type == 'pen':
+        cursor = window.get_system_mouse_cursor(window.CURSOR_CROSSHAIR)
+    elif type == 'default':
+        cursor = ColoredMouseCursor(board.active_color)
     else:
-        cursor = window.get_system_mouse_cursor(type)
+        raise ValueError("Unsupported cursor: " + type)
     window.set_mouse_cursor(cursor)
